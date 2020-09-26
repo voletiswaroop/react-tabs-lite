c55805d33ac989046a48ea7c8627ed774a7ef2d2
Swaroop Gupta Voleti <vswaroop@athenahealth.com>
2020-09-26 12:01:40 +0530

    Updates

---
+++ precache-manifest.e48898dbe25855e4994e41aafd943163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021da9e374531149e633f9de49534c0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2f4ebeacbf9df7e1f66",
+    "url": "./static/css/main.6bc48a50.chunk.css"
+  },
+  {
+    "revision": "ee62f127ddfb23b6c602",
+    "url": "./static/js/2.0267d877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0267d877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f4ebeacbf9df7e1f66",
+    "url": "./static/js/main.60d38076.chunk.js"
+  },
+  {
+    "revision": "adb34275e767dab5241f",
+    "url": "./static/js/runtime-main.66f37f4f.js"
+  }
+]);
